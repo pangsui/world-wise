1243ce3011ae050863c56f897f610236f3c99a37
pangsui <upangsuilingeh@gmail.com>
2024-11-01 09:46:24 +0100

    project set up commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+import Products from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import "./index.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Products />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App nav element</p>
+    </div>
+  );
+}
+
+export default AppLayout;
